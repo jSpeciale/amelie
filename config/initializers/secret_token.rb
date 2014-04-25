331a4660356be02546b0b1c69e4d15bc3fbfd6e8
Joao Speciale <joao.speciale@gmail.com>
2014-04-24 22:05:33 -0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AChocolatier::Application.config.secret_key_base = '1e0aab2199961d1b7cba4de94e99f4356c97a15672ace94c029b9655a2c450ba007bc090d1fed66b39282a5d479549ed9f69eb8e59ba37e589596198556d905c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AChocolatier::Application.config.session_store :cookie_store, key: '_aChocolatier_session'
